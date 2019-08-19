9433fe0afc35948a1a39f01dfb9ca678303c6c75
CK <kainingc@usc.edu>
2019-08-19 10:07:07 -0700

    first commit

---
+++ src/com/CK/Main.java
@@ -0,0 +1,33 @@
+package com.CK;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+class Solution {
+    public boolean isUgly(int num) {
+        if (num <= 0) return false;
+        if (num == 1) return true;
+
+        while (num > 1) {
+            if (num % 2 == 0){
+                num = num / 2;
+            }
+
+            else if (num % 3 == 0){
+                num = num / 3;
+            }
+
+            else if (num % 5 ==0) {
+                num = num /5;
+            }
+
+            else return false;
+        }
+
+        return true;
+    }
+}
